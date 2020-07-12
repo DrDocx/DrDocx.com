d18bc0375a7289c0f7d64d0966134539e34e77c6
Faiz Surani <faiz.surani@gmail.com>
2020-07-12 12:57:07 -0700

    Create DrDocx docusaurus website

---
+++ docusaurus.config.js
@@ -0,0 +1,88 @@
+module.exports = {
+  title: 'DrDocx',
+  tagline: 'Quickly organize patient data and generate Word reports',
+  url: 'https://drdocx.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'DrDocx', // Usually your GitHub org/user name.
+  projectName: 'DrDocx.com', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'DrDocx',
+      logo: {
+        alt: 'DrDocx Logo',
+        src: 'img/icon.ico',
+      },
+      links: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/DrDocx',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/DrDocx',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} DrDocx.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'doc1',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
