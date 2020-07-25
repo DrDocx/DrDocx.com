259eeb2e65ad7b2ffdf82ce4c089f7763b6c2c28
Faiz Surani <faiz.surani@gmail.com>
2020-07-25 14:03:24 -0700

    Deploy website - based on dc8c2849dcb5f7bf61a5fea680011ebd71557822

---
+++ 8be5b89e.9bed3c8a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{112:function(e){e.exports=JSON.parse('{"desktop":{"allTagsPath":"/blog/tags","slug":"desktop","name":"desktop","count":1,"permalink":"/blog/tags/desktop"},"release":{"allTagsPath":"/blog/tags","slug":"release","name":"release","count":1,"permalink":"/blog/tags/release"}}')}}]);
